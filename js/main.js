0f6a00cb3a2fa6689633f758e1339a60c13a621b
Oksil-Joo <bbkke07@gmail.com>
2021-10-23 04:57:31 -0400

    alittleModifyJSfile

--- js/main.js
+++ js/main.js
@@ -1,26 +1,30 @@
 (() => {
-    const theTeam = document.querySelector("#team-members"),
+    const   theTeam = document.querySelector("#team-members"),
             theTemplate = document.querySelector("#bio-template").content;
-            
-    let buttons = document.querySelectorAll("button");
-
-    function showData() {
-        let key = this.dataset.key;
-
-        let headline = document.querySelector("h1");
-
-        headline.textContent = data[key].name;
-    }
+    // let buttons = document.querySelectorAll("button");
+          
 
     function getData() {
         fetch("./data.json")
         .then(res => res.json()) //unpach the API
         .then(data => {
             console.table(data);
-
-            buildTeam(info);
+        
+            buildTeam(data);
         })
+        .catch(error => console.error(error));
     }
+
+//     function showData() {
+        
+//         let key = this.dataset.key;
+
+//         let headline = document.querySelector("h1");
+
+//         headline.textContent = data[key].name;
+// };
+
+
 
     function buildTeam(info) {
        let team = Object.keys(info);
@@ -29,16 +33,18 @@
            let panel = theTemplate.cloneNode(true),
                 memberInfo = panel.firstElementChild.children;
 
-            memberInfo[0].querySelector('img').src = `images/${info[person].biopic}`;
+            memberInfo[0].querySelector('img').src = `images/${info[person].biopic}}`;
             memberInfo[1].textContent = info[person].name;
             memberInfo[2].textContent = info[person].role;
             memberInfo[3].textContent = info[person].desc;
 
             theTeam.appendChild(panel);
        })
-    }
-    //invoke the getData function
+    };
+
     getData();
 
-    buttons.forEach(button => button.addEventListener("click", showData));
+    // buttons.forEach(button => button.addEventListener("click", showData));
+    
+
 })();
